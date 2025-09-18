d63d860eb6c476c5c4d65929ec2e8637a4d8e7d4
Azim-Khoja <az.makhmudoff@gmail.com>
2025-09-18 10:48:03 +0500

    hw-1.py added

---
+++ lesson-1/homework/hw-1.py
@@ -0,0 +1,46 @@
+# 1. Given a side of square. Find its perimeter and area.
+A = 8
+B = 6
+
+P = A + B
+Y = A * B
+
+print("Kvadratning perimetri:", P)
+print("Kvadratning yuzi:", Y)
+# ==================================================================================
+
+# 2. Given diameter of circle. Find its length.
+D = 7
+pi = 3.14159
+
+U = D * pi
+print("Aylananing uzunligi:", U)
+# ==================================================================================
+
+# 3.Given two numbers a and b. Find their mean.
+a = 5
+b = 9
+
+M = (a + b) / 2
+print("O'rta qiymat:", M)
+# ==================================================================================
+
+# 4. Given two numbers a and b. Find their sum, product and square of each number.
+a = 9
+b = 6
+
+summary = a + b
+product = a * b
+squareA = a ** 2
+squareB = b ** 2
+
+print("Yig'indisi:", summary)
+print("Ko'paytmasi:", product)
+print("a-ning kvadrati:", squareA)
+print("b-ning kvadrati:", squareB)
+
+
+
+
+
+
